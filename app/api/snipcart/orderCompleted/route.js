aeeb5686d7d9f73279160a0a56af77380362c2b2
illoquehambre <illoquehambre@gmail.com>
2024-09-23 10:09:22 +0200

    webhook try

--- app/api/snipcart/orderCompleted/route.js
+++ app/api/snipcart/orderCompleted/route.js
@@ -2,15 +2,17 @@
 export async function POST(req) {
 
     try {
-        const order = req.body; // Aquí recibimos los datos del pedido de Snipcart
+      
+        
+        const data = await req.json(); // Aquí recibimos los datos del pedido de Snipcart
         console.log(req);
-        console.log(order);
+        console.log(JSON.stringify(data));
         // Extraer información de facturación, envío y productos del carrito
         const billingInfo = order.billingAddress;
         const shippingInfo = order.shippingAddress;
         const products = order.items;
 
-        return new Response(JSON.stringify(req), {
+        return new Response(JSON.stringify(data), {
             headers: { 'Content-Type': 'application/json' },
         });        
 } catch (error) {
